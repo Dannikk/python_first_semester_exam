2fd4ff344a1369e1a2847e49e5156893f245da94
Dmitroved <dmitroved@gmail.com>
2021-01-15 16:29:18 +0300

    generator in class

--- src/directory_reader.py
+++ src/directory_reader.py
@@ -19,11 +19,17 @@ def print_iter(reader):  # a function decorator
 
 
 @print_iter
-def DirReader(dir):  # To read files in a directory
-    walk = os.walk(dir)
-    for root, dirs, files in walk:
-        if files:
-            for file in files:
-                if file.split('.')[
-                    Var.format_position_in_split] == Var.format_req:  # If the file matches the format we need
-                    yield root + "\\" + file  # giving the canonical path
+class DirReader:  # To read files in a directory
+    def __init__(self, dir):
+        self.walk = os.walk(dir)
+
+    def work(self):
+        for root, dirs, files in self.walk:
+            if files:
+                for file in files:
+                    if file.split('.')[
+                        Var.format_position_in_split] == Var.format_req:  # If the file matches the format we need
+                        yield root + "\\" + file  # giving the canonical path
+
+    def __iter__(self):
+        return self.work()
