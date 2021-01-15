02de95d5461db3be8cefe36b35455a2883642755
Dmitroved <dmitroved@gmail.com>
2021-01-15 16:17:29 +0300

    done with generators

--- src/directory_reader.py
+++ src/directory_reader.py
@@ -10,9 +10,7 @@ class Var:
 def print_iter(reader):  # a function decorator
     @functools.wraps(reader)
     def dir_reader_decorator(*args, **kwargs):  # The class that will contain the wrapped class
-        # print("init")
         inner_reader = reader(*args, **kwargs)
-        # print(type(inner_reader))
         for result in inner_reader:
             print(result)
             yield result
@@ -24,41 +22,8 @@ def print_iter(reader):  # a function decorator
 def DirReader(dir):  # To read files in a directory
     walk = os.walk(dir)
     for root, dirs, files in walk:
-        # root, dirs, files = next(walk)
         if files:
             for file in files:
                 if file.split('.')[
                     Var.format_position_in_split] == Var.format_req:  # If the file matches the format we need
-                    # self.files_remaining = files[files.index(file) + 1:]  # We remember the remaining files
                     yield root + "\\" + file  # giving the canonical path
-
-# @print_iter
-# class DirReader:  # To read files in a directory
-#     def __init__(self, dir):  # Getting the path to the directory
-#         self.dir = dir
-#         self.walk = os.walk(dir)
-#         print("init")
-#         # self.files_remaining = []  # To write unread files
-#
-#     def work(self):
-#         while True:
-#             root, dirs, files = next(self.walk)
-#             if files:
-#                 for file in files:
-#                     if file.split('.')[
-#                         Var.format_position_in_split] == Var.format_req:  # If the file matches the format we need
-#                         # self.files_remaining = files[files.index(file) + 1:]  # We remember the remaining files
-#                         yield root + "\\" + file  # giving the canonical path
-# if len(self.files_remaining) == 0:  # If there are no unread messages left, take the next step
-#     root, dirs, files = next(self.walk)
-# else:
-#     files = self.files_remaining
-# if files:
-#     for file in files:
-#         if file.split('.')[Var.format_position_in_split] == Var.format_req:  # If the file matches the format we need
-#             self.files_remaining = files[files.index(file) + 1:]  # We remember the remaining files
-#             return root + "\\" + file  # giving the canonical path
-# return next(self)
-
-# def __iter__(self):  # So that you can run through the for loop
-#     return self
