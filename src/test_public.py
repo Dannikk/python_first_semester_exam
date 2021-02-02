fd06fca95102115fa20f145a586647c74e9599a0
Dannikk <nikita.danilov0103@gmail.com>
2021-02-02 03:46:54 +0300

    Edited test_public.py

--- src/test_public.py
+++ src/test_public.py
@@ -1,10 +1,9 @@
 import sys
 import unittest
-from src import directory_reader as d_r
-
-
+#from src import directory_reader as d_r
+from src.directory_reader import *
 
 class TestF(unittest.TestCase):
     def test_DirReader(self):
-        for file in d_r.DirReader("test_dir"):
+        for file in DirReader("test_dir"):
             self.assertEqual(file, "test_dir" + "\\test.txt")
